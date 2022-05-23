3fccad70fc54960197f105dc22d1f2cda9623f22
trandunz <54924605+trandunz@users.noreply.github.com>
2022-05-23 20:35:31 +1200

    + Script_Player
    + Script_TaskInteractable

---
+++ OCD_Game/Assets/Scripts/Tasks/Script_TaskInteractable.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_TaskInteractable : MonoBehaviour
+{
+    #region Private
+    [SerializeField] GameObject UIPanel;
+    #endregion
+
+    #region Public
+    public void Interact()
+    {
+        UIPanel.SetActive(true);
+    }
+    #endregion
+}

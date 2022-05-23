6b6ecbdcc35c69615e9bf30ebfd9e012f3e2372d
trandunz <54924605+trandunz@users.noreply.github.com>
2022-05-23 20:12:45 +1200

    + Basic Character Motor

--- OCD_Game/Assets/Scripts/Player/Script_FirstPersonMotor.cs
+++ OCD_Game/Assets/Scripts/Player/Script_FirstPersonMotor.cs
@@ -7,24 +7,55 @@ public class Script_FirstPersonMotor : MonoBehaviour
     #region Private
     Camera FirstPersonCamera;
     GameObject Mesh;
+    CharacterController CharacterController;
+
+    float xMouseRotation = 0.0f;
+    float yMouseRotation = 0.0f;
+
+    [SerializeField] float MovementSpeed = 10.0f;
+    [SerializeField] float GravityStrength = -9.81f;
+    [SerializeField] float LookSensitivity = 1.0f;
 
     private void Start()
     {
         FirstPersonCamera = GetComponentInChildren<Camera>();
         Mesh = GetComponentInChildren<MeshRenderer>().gameObject;
+        CharacterController = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
     }
-    Vector2 GetMovementInput()
+    private void Update()
     {
-        Vector2 input = Vector2.zero;
+        Vector3 input = Quaternion.Euler(0.0f, xMouseRotation, 0.0f) * GetMovementInput();
+        ApplyGravity();
+        HandleFPSCamera();
+        if (input.magnitude > 0)
+        {
+            CharacterController.Move(input * MovementSpeed * Time.deltaTime);
+        }
+    }
+    private Vector3 GetMovementInput()
+    {
+        Vector3 input = Vector3.zero;
         if (Input.GetKey(KeyCode.W))
-            input.y += 1;
+            input.z += 1;
         if (Input.GetKey(KeyCode.S))
-            input.y -= 1;
+            input.z -= 1;
         if (Input.GetKey(KeyCode.A))
             input.x -= 1;
         if (Input.GetKey(KeyCode.D))
             input.x += 1;
         return input.normalized;
+    }
+    private void ApplyGravity()
+    {
+        CharacterController.Move(new Vector3(0, GravityStrength, 0) * Time.deltaTime);
+    }
+    private void HandleFPSCamera()
+    {
+        xMouseRotation += Input.GetAxisRaw("Mouse X") * LookSensitivity;
+        yMouseRotation -= Input.GetAxisRaw("Mouse Y") * LookSensitivity;
+        yMouseRotation = Mathf.Clamp(yMouseRotation, -85, 85);
+        FirstPersonCamera.transform.localEulerAngles = new Vector3(yMouseRotation, xMouseRotation, 0.0f);
     }
     #endregion
 }

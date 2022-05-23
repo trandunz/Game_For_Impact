202c4e432bbe7b5283991e5d3a8af5fe82c4a216
trandunz <54924605+trandunz@users.noreply.github.com>
2022-05-23 19:32:10 +1200

    + Reatime Csg
    + Player Prefeb And Motor Started

---
+++ OCD_Game/Assets/Scripts/Player/Script_FirstPersonMotor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_FirstPersonMotor : MonoBehaviour
+{
+    #region Private
+    Camera FirstPersonCamera;
+    GameObject Mesh;
+
+    private void Start()
+    {
+        FirstPersonCamera = GetComponentInChildren<Camera>();
+        Mesh = GetComponentInChildren<MeshRenderer>().gameObject;
+    }
+    Vector2 GetMovementInput()
+    {
+        Vector2 input = Vector2.zero;
+        if (Input.GetKey(KeyCode.W))
+            input.y += 1;
+        if (Input.GetKey(KeyCode.S))
+            input.y -= 1;
+        if (Input.GetKey(KeyCode.A))
+            input.x -= 1;
+        if (Input.GetKey(KeyCode.D))
+            input.x += 1;
+        return input.normalized;
+    }
+    #endregion
+}

211e8157ddddfd48e1f63f31c0956522ce162fcf
JoshB9999 <88113818+JoshB9999@users.noreply.github.com>
2022-05-24 18:29:06 +1200

    + Prototype Task
    added a prototype task in Josh Test Scene

---
+++ OCD_Game/Assets/Scripts/Tasks/Script_PrototypeTaskDemo.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Script_PrototypeTaskDemo : MonoBehaviour, IPointerClickHandler
+{
+    [SerializeField] Color ClickedColor = Color.green;
+    [SerializeField] Image Button;
+    [SerializeField] Script_PrototypeCloseUI UIManager;
+    void Start()
+    {
+        UIManager = GetComponentInParent<Script_PrototypeCloseUI>();
+    }
+    public void OnPointerClick(PointerEventData pointerEventData)
+    {
+        if (Button.color != ClickedColor)
+        {
+            Button.color = ClickedColor;
+            UIManager.Increment();
+        }
+    }
+}
---
+++ OCD_Game/Assets/Scripts/UI/Script_PrototypeCloseUI.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_PrototypeCloseUI : MonoBehaviour
+{
+    int iCounter = 0;
+    [SerializeField] float fTimeTillClose = 0.2f;
+    [SerializeField] Script_Player Player;
+    [SerializeField] bool UnlockCursor;
+    float fCounterTillClose = 0.0f;
+
+    public void Increment()
+    {
+        iCounter++;
+    }
+
+    public void UnlockCursorFunction()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        UnlockCursor = false;
+        Player.SetInteracting(true);
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if (UnlockCursor)
+        {
+            UnlockCursorFunction();
+        }
+
+        if (iCounter >= 6)
+        {
+            fCounterTillClose += Time.deltaTime;
+            if (fCounterTillClose >= fTimeTillClose)
+            {
+                gameObject.SetActive(false);
+                Player.SetInteracting(false);
+            }
+        }
+    }
+}

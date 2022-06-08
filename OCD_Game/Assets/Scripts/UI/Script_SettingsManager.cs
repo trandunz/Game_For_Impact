0d2c5425114a083651bf595a24cfeeae59ccd6f6
JoshB9999 <88113818+JoshB9999@users.noreply.github.com>
2022-06-08 15:11:29 +1200

    added in basic settings sliders
    need to tie it to open on button press

---
+++ OCD_Game/Assets/Scripts/UI/Script_SettingsManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Script_SettingsManager : MonoBehaviour
+{
+    public static float AudioVolume;
+    public static float MouseSensitivity;
+    public static Vector2 ScreenSize;
+
+    [SerializeField] Slider AudioSlider;
+    [SerializeField] Text AudioNumber;
+
+    [SerializeField] Slider SensitivitySlider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // load from settings file
+    }
+
+    public void UpdateAudio()
+    {
+        float fSliderNumber = AudioSlider.value * 10;
+        int iSliderNumber = (int)fSliderNumber;
+        AudioNumber.text = iSliderNumber.ToString();
+        AudioVolume = AudioSlider.value;
+    }
+
+    public void UpdateSensitivty()
+    {
+        MouseSensitivity = (int)SensitivitySlider.value;
+    }
+}

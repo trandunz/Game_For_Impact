6deb8bc7b82ba7d031e7153622bad9bd0f01f5b5
JoshB9999 <88113818+JoshB9999@users.noreply.github.com>
2022-06-24 19:30:01 +1200

    added functions to toggle light

--- OCD_Game/Assets/Scripts/Script_Rotate.cs
+++ OCD_Game/Assets/Scripts/Script_Rotate.cs
@@ -11,21 +11,20 @@ public class Script_Rotate : MonoBehaviour
     // variables
     [SerializeField] float fRotateSpeed;
     [SerializeField] Light PointLight1;
-    [SerializeField] Script_Alarm Alarm;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        Alarm = FindObjectOfType<Script_Alarm>();
-    }
 
     // Update is called once per frame
     void Update()
     {
         transform.Rotate(0, fRotateSpeed * Time.deltaTime, 0);
+    }
 
-        // makes the point light range based on the volume of the alarm sound, making the light more
-        // intense as the alarm gets more intense
-        PointLight1.range = Alarm.fVolume * 50;
+    public void EnableLight()
+    {
+        PointLight1.gameObject.SetActive(true);
+    }
+
+    public void DisableLight()
+    {
+        PointLight1.gameObject.SetActive(false);
     }
 }

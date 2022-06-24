27877b642a123faa4b1e12c9924f1ccd6ce726ae
JoshB9999 <88113818+JoshB9999@users.noreply.github.com>
2022-06-24 12:44:06 +1200

    made settings load on start

---
+++ OCD_Game/Assets/Scripts/Script_LoadSettingInit.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Script_LoadSettingInit : MonoBehaviour
+{
+    [SerializeField] float PanStereo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadPrefs();
+
+        AudioListener.volume = Script_SettingsManager.AudioVolume;
+
+        UpdatePan();
+
+        Script_FirstPersonMotor Player = FindObjectOfType<Script_FirstPersonMotor>();
+        if (Player != null)
+        {
+            Player.UpdateSensitivity();
+        }
+
+        Destroy(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdatePan()
+    {
+        float fSliderValue = (PanStereo - 5) / 10;
+        foreach (AudioSource source in FindObjectsOfType<AudioSource>())
+        {
+            source.panStereo = fSliderValue;
+        }
+    }
+
+
+    void LoadPrefs()
+    {
+        Script_SettingsManager.AudioVolume = PlayerPrefs.GetFloat("Volume", 1.0f);
+        Script_SettingsManager.MouseSensitivity = PlayerPrefs.GetFloat("Sensitivity", 1.0f);
+        PanStereo = PlayerPrefs.GetFloat("PanStereo", 5.0f);
+    }
+}

da2742c2a47156d5530780bb4cd41090ae665904
JoshB9999 <88113818+JoshB9999@users.noreply.github.com>
2022-06-24 13:20:58 +1200

    added a settings menu to the main menu

---
+++ OCD_Game/Assets/Scripts/Script_MainMenuManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_MainMenuManager : MonoBehaviour
+{
+    [SerializeField] GameObject MainMenu;
+    [SerializeField] GameObject SettingsMenu;
+
+    public void OpenSettings()
+    {
+        MainMenu.SetActive(false);
+        SettingsMenu.SetActive(true);
+    }
+
+    public void CloseSettings()
+    {
+        MainMenu.SetActive(true);
+        SettingsMenu.SetActive(false);
+    }
+}

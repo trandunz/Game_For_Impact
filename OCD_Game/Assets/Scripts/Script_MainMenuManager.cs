8a09751b52e7a483d2d7910aa35c35426271ca94
JoshB9999 <88113818+JoshB9999@users.noreply.github.com>
2022-06-24 17:24:45 +1200

    added trigger warning

--- OCD_Game/Assets/Scripts/Script_MainMenuManager.cs
+++ OCD_Game/Assets/Scripts/Script_MainMenuManager.cs
@@ -5,11 +5,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Script_MainMenuManager : MonoBehaviour
 {
+    static bool PlayedTriggerWarning = false;
+    [SerializeField] float fTriggerWarningTime = 10.0f;
+    float fTriggerWarningCounter = 0.0f;
+    [SerializeField] float fDifferInAlpha;
+
+    // screens
+    [SerializeField] GameObject TriggerWarning;
+    [SerializeField] Text TriggerText;
     [SerializeField] GameObject MainMenu;
     [SerializeField] GameObject SettingsMenu;
+
+    private void Start()
+    {
+        TriggerText = TriggerWarning.GetComponentInChildren<Text>();
+        fDifferInAlpha = TriggerText.color.r * fTriggerWarningTime * 0.2f;
+        if (PlayedTriggerWarning)
+        {
+            MainMenu.SetActive(true);
+            TriggerWarning.SetActive(false);
+            fTriggerWarningCounter = fTriggerWarningTime;
+        }
+    }
+
+    private void Update()
+    {
+        if (fTriggerWarningTime > fTriggerWarningCounter)
+        {
+            fTriggerWarningCounter += Time.deltaTime;
+
+            if (Input.GetMouseButtonDown(0) && fTriggerWarningCounter > 0.5f && fTriggerWarningCounter < fTriggerWarningTime * 0.8f)
+            {
+                if (TriggerWarning.activeSelf)
+                {
+                    fTriggerWarningCounter = fTriggerWarningTime * 0.8f;
+                }
+            }
+
+            if (fTriggerWarningCounter > fTriggerWarningTime * 0.9)
+            {
+                TriggerText.color = new Color(TriggerText.color.r, TriggerText.color.g, TriggerText.color.b, TriggerText.color.a - fDifferInAlpha * Time.deltaTime);
+            }
+
+            if (fTriggerWarningTime <= fTriggerWarningCounter)
+            {
+                MainMenu.SetActive(true);
+                TriggerWarning.SetActive(false);
+                PlayedTriggerWarning = true;
+            }
+        }
+    }
 
     /// <summary>
     /// enables the setting menu, and disables the main menu

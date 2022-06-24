f930e2163df155998fbd559063f9fbd18e350585
JoshB9999 <88113818+JoshB9999@users.noreply.github.com>
2022-06-24 15:14:21 +1200

    added lights + added Home Button to main menu settings

---
+++ OCD_Game/Assets/Scripts/Script_Rotate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_Rotate : MonoBehaviour
+{
+    [SerializeField] float fRotateSpeed;
+    [SerializeField] Light PointLight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, fRotateSpeed * Time.deltaTime, 0);
+    }
+}
